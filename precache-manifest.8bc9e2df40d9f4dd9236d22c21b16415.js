6bfaeb463f23eaf1f26cbe4458ca46e35a62620c
Kienvic2803 <kienct@elcom.com.vn>
2024-03-27 10:35:32 +0700

    deploy

---
+++ precache-manifest.8bc9e2df40d9f4dd9236d22c21b16415.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b84af39f48eda2c7ffe",
+    "url": "/zing-mp3-vue/css/app.6aee54c9.css"
+  },
+  {
+    "revision": "9423863a8854ca753587",
+    "url": "/zing-mp3-vue/css/chunk-3b7f395c.3e408944.css"
+  },
+  {
+    "revision": "e739a92e3466fc39e3b5",
+    "url": "/zing-mp3-vue/css/chunk-vendors.6b92dc4c.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/zing-mp3-vue/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/zing-mp3-vue/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "0197cca312f622e2991a40f1bf280837",
+    "url": "/zing-mp3-vue/index.html"
+  },
+  {
+    "revision": "3b84af39f48eda2c7ffe",
+    "url": "/zing-mp3-vue/js/app.6719b727.js"
+  },
+  {
+    "revision": "9423863a8854ca753587",
+    "url": "/zing-mp3-vue/js/chunk-3b7f395c.4091d438.js"
+  },
+  {
+    "revision": "e739a92e3466fc39e3b5",
+    "url": "/zing-mp3-vue/js/chunk-vendors.cf4e4511.js"
+  },
+  {
+    "revision": "4efd1c0bc205bfa3fb39dc29e36710ff",
+    "url": "/zing-mp3-vue/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/zing-mp3-vue/robots.txt"
+  }
+]);
